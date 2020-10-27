a79f203bac8c212349b1a8fec21e48538b770b8d
iroxenaki <68063300+iroxenaki@users.noreply.github.com>
2020-10-27 20:28:20 +0200

    Update Player.java

--- Player.java
+++ Player.java
@@ -21,6 +21,55 @@ public class Player {
 		this.y = y;
 	}
 	
+	public void setPayerId(int plplayerId) {
+		playerId = plplayerId;
+	}
+	
+	public int getPayerId() {
+		return playerId;
+	}
+	
+	public void setName(String plname) {
+		name = plname;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setBoard(Board plboard) {
+		board = plboard;
+	}
+	
+	public Board getBoard() {
+		return board;
+	}
+	
+	public void setScore(int plscore) {
+		score = plscore;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void setX(int plx) {
+		x = plx;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setY(int ply) {
+		y = ply;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	
 	int[] move(int id) {
 		
 		return;
